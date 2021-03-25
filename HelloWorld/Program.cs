598e63fd55c7fdbedda35eef98bafef4ad8e6ec0
Jaxon Ray Petersen <thatCashBar@gmail.com>
2021-03-25 12:37:48 -0400

    Finished Checkpoint Pt.2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 
 namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+      static public string DefaultConnectionString { get; } = @"Server=(localdb)\\mssqllocaldb;Database=SampleData-0B3B0919-C8B3-481C-9833-36C21776A565;Trusted_Connection=True;MultipleActiveResultSets=true";
+      static IReadOnlyDictionary<string, string> DefaultConfigurationStrings { get; } = new Dictionary<string, string>()
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-        }
+            ["Profile:UserName"] = Environment.UserName,
+            [$"AppConfiguration:ConnectionString"] = DefaultConnectionString,
+        };
+      static public IConfiguration Configuration { get; set; }
+      public static void Main(string[] args = null)
+      {
+          ConfigurationBuilder configurationBuilder =new ConfigurationBuilder();
+
+          configurationBuilder.AddInMemoryCollection(DefaultConfigurationStrings);
+          configurationBuilder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+          Configuration = configurationBuilder.Build();
+
+          Console.SetWindowSize(Int32.Parse(Configuration["AppConfiguration:MainWindow:Width"]), (Int32.Parse(Configuration["AppConfiguration:MainWindow:Height"])));
+          Console.WriteLine($"Hello {Configuration["Profile:UserName"]}");
+
+          Console.WriteLine($"{Configuration["message"]}");
+          Console.ReadKey();
+      }
     }
 }
